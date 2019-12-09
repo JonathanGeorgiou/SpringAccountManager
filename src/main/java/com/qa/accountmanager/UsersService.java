f2d571515863d45b70427198544e97c03642ebfa
Chloe Adcock <Chloe@Chloes-MacBook-Air.local>
2019-12-09 16:51:54 +0000

    First commit

--- src/main/java/com/qa/accountmanager/UsersService.java
+++ src/main/java/com/qa/accountmanager/UsersService.java
@@ -1,5 +1,7 @@
 package com.qa.accountmanager;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,12 +9,32 @@ import org.springframework.stereotype.Service;
  * UsersService
  */
 
-
 @Service
  public class UsersService {
 
     @Autowired
     private UserRepo userRepo;
-
     
+    public Users createUser (Users user) {
+    	return this.userRepo.save(user);
+    }
+    
+    public List<Users> readUsers() {
+    	return this.userRepo.findAll();
+    	
+    }
+    
+    public void updateUser(Users user, Long id) {
+    	Users toUpdate = this.userRepo.getOne(id);
+    	toUpdate.setFirstName(user.getFirstName());
+    	toUpdate.setLastName(user.getLastName());
+    	toUpdate.setAccountNumber(user.getAccountNumber());
+    	toUpdate.setWinnings(user.getWinnings());
+    }
+    
+    public void deleteUser(Long id) {
+    	this.userRepo.deleteById(id);
+    	
+    }
+
 }
