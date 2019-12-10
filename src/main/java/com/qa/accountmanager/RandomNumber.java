c3a3b20fcc1e861e8dedc5590ecc2c9d05474f21
LiamMcIvor <liam.mcivor1@gmail.com>
2019-12-10 11:06:42 +0000

    Refactored Random Number Feature

--- src/main/java/com/qa/accountmanager/RandomNumber.java
+++ src/main/java/com/qa/accountmanager/RandomNumber.java
@@ -2,40 +2,40 @@ package com.qa.accountmanager;
 
 import java.util.Random;
 
-//import org.springframework.util.StringUtils;
-//import org.apache.commons.lang.StringUtils;
-
 public class RandomNumber {
-
-	static Random randomGenerator = new Random();
-	static Random r = new Random();
-	static char c = (char)(r.nextInt(3) + 'a');
 	
-	public static void main(String[] args) {
-		System.out.println(get6DigitRandNum());
-		System.out.println(get8DigitRandNum());
-		System.out.println(get10DigitRandNum());
+	Random randomGenerator = new Random();
+	char c = (char)(randomGenerator.nextInt(3) + 'a');
+	
+	public String randNumGen() {
+		int n = randomGenerator.nextInt(3);
+		switch(n) {
+		case(0):
+			return get6DigitRandNum();
+		case(1):
+			return get8DigitRandNum();
+		case(2):
+			return get10DigitRandNum();
+		default:
+			return null;
+		}
 	}
 	
-	public static String get6DigitRandNum(){
+	public String get6DigitRandNum(){
 		int random = randomGenerator.nextInt(999999) + 1;
-		String padded = String.format("%06d" , random);
-		String concat = c + padded;
+		String concat = c + String.format("%06d" , random);
 		return concat; 
 	}
 	
-	public static String get8DigitRandNum(){
+	public String get8DigitRandNum(){
 		int random = randomGenerator.nextInt(99999999) + 1;
-		String padded = String.format("%08d" , random);
-		String concat = c + padded;
+		String concat = c + String.format("%08d" , random);
 		return concat; 
 	}
 	
-	public static String get10DigitRandNum(){
+	public String get10DigitRandNum(){
 		long random = randomGenerator.nextInt((int) 9999999999L) + 1;
-		String padded = String.format("%010d" , random);
-		String concat = c + padded;
+		String concat = c + String.format("%010d" , random);
 		return concat; 
-		
 	}
 }
