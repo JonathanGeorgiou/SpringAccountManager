01bf0e07795201cd4e40a207e3d5442d3ffb88eb
Rebekah Radcliffe <rebekah.zoe.radcliffe@gmail.com>
2019-12-10 11:12:11 +0000

    First Commit with slight edit to users service

---
+++ src/main/java/com/qa/accountmanager/UsersController.java
@@ -0,0 +1,39 @@
+package com.qa.accountmanager;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class UsersController {
+	
+	    private UsersService service;
+	    
+	    @PostMapping("/createUser")
+	    public Users createUser (@RequestBody Users user) {
+	    	return this.service.createUser(user);
+	    }
+	    
+	    @GetMapping("/getUsers")
+	    public List<Users> readUsers() {
+	    	return this.service.readUsers();
+	    	
+	    }
+	    @PutMapping("/updateUser")
+	    public Users updateUser(@PathParam ("id")Long id, @RequestBody Users user ) {
+	    	return this.service.updateUser(user, id);
+	    	
+	    }
+	    @DeleteMapping("/deleteUser/{id}")
+	    public void deleteUser(@PathVariable Long id) {
+	    	this.service.deleteUser(id);
+	    }
+	    	
+
+}
