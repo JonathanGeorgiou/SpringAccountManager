e95f0cec1a599516075f037b2b504c9622dfbf23
Harr1s96 <hanleyharris96@gmail.com>
2019-12-10 15:55:16 +0000

    test commit

--- src/test/java/com/qa/accountmanager/CreateUserTest.java
+++ src/test/java/com/qa/accountmanager/CreateUserTest.java
@@ -1,5 +1,42 @@
 package com.qa.accountmanager;
 
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * CreateUserTest
+ */
+
+@RunWith(SpringRunner.class)
+public class CreateUserTest {
+
+	@InjectMocks
+	private UsersService service;
+
+	@Mock
+	private UserRepo repo;
+
+	@Test
+	public void createUserTest() {
+
+		Users myUser = new Users();
+
+		Mockito.when(repo.save(myUser)).thenReturn(myUser);
+		assertTrue("Harris", this.service.createUser(myUser));
+
+
+	}
+
+
+
+	
+}
+
 
 
 
