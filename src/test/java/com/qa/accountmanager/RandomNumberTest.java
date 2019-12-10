10f168d63ce976a9e2ca655da65684782a2ce39b
JonathanGeorgiou <j.georgiou94@gmail.com>
2019-12-10 15:29:42 +0000

    updated getUserTest

--- src/test/java/com/qa/accountmanager/RandomNumberTest.java
+++ src/test/java/com/qa/accountmanager/RandomNumberTest.java
@@ -1,26 +1,26 @@
-package com.qa.accountmanager;
+// package com.qa.accountmanager;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
-import junit.framework.Assert;
+// import junit.framework.Assert;
 
-class RandomNumberTest {
+// class RandomNumberTest {
 
-	@Test
-	void test() {
-		// RandomNumber random = new RandomNumber();
-		// boolean isItTrue = (random.c == 'a' || random.c == 'b' || random.c == 'c');
-		// boolean firstLetterIsTrue = (random.randNumGen().charAt(0) == 'a'||random.randNumGen().charAt(0) == 'b'
-		// 		||random.randNumGen().charAt(0) == 'c');
+// 	@Test
+// 	void test() {
+// 		RandomNumber random = new RandomNumber();
+// 		boolean isItTrue = (random.c == 'a' || random.c == 'b' || random.c == 'c');
+// 		boolean firstLetterIsTrue = (random.randNumGen().charAt(0) == 'a'||random.randNumGen().charAt(0) == 'b'
+// 				||random.randNumGen().charAt(0) == 'c');
 		
-		// assertTrue(isItTrue);
-		// assertTrue(firstLetterIsTrue);
+// 		assertTrue(isItTrue);
+// 		assertTrue(firstLetterIsTrue);
 
-		// assertEquals(random.get6DigitRandNum().length(), 7);
-		// assertEquals(random.get8DigitRandNum().length(), 9);
-		// assertEquals(random.get10DigitRandNum().length(), 11);
-	}
+// 		assertEquals(random.get6DigitRandNum().length(), 7);
+// 		assertEquals(random.get8DigitRandNum().length(), 9);
+// 		assertEquals(random.get10DigitRandNum().length(), 11);
+// 	}
 
-}
+// }
